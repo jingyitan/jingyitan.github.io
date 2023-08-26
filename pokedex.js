fe820ea1b10f2e03dbb2ced26208d2a83b22d39f
71ng_Y1 <142076999+jingyitan@users.noreply.github.com>
2023-08-26 09:51:03 +0800

    Update pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -62,3 +62,32 @@ function updatePokemon(){
     //update description
     document.getElementById("pokemon-description").innerText = pokedex[this.id]["desc"];
 }
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = user.name
+      body.textContent = user.email
+      userCardContainer.append(card)
+      return { name: user.name, email: user.email, element: card }
+    })
+  })
